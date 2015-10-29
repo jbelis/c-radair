ba78842dcc432c6d39caf52759c9d0f8e831e704
Jacques Belissent <jbelis@gmail.com>
2015-10-29 13:57:04 +0100

    added services folder

---
+++ www/services/push.js
@@ -0,0 +1,27 @@
+(function () {
+  "use strict";
+
+  var app = angular.module('cradair', ['ionic']);
+
+  app.service('Push', function () {
+    var registrationId;
+
+    return {
+      init: function() {
+        var push = new Ionic.Push({
+          "debug": true
+        });
+
+        push.register(function(token) {
+          registrationId = token;
+          console.log("Device token:",token.token);
+        });
+
+      },
+      getRegistrationId: function() {
+        return registrationId;
+      }
+    }
+  });
+
+})();
