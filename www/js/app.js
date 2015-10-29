dacdec80976fe35cec4f8c0ce6457e0574b6cd8a
henry <henry.nomichith@data-publica.com>
2015-10-29 12:56:48 +0100

    FIRST COMMIT !

--- www/js/app.js
+++ www/js/app.js
@@ -3,17 +3,34 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('cradair', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider.state('login', {
+      url: '/login',
+      templateUrl: 'modules/login/login.html',
+      controller: 'LoginCtrl'
+      })
+
+      .state('notificationsList', {
+        url: '/notificationsList',
+        templateUrl: 'modules/notifications-list/notifications-list.html',
+        controller: 'NotificationsListCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/login');
   });
-})
---
+++ www/modules/notifications-list/NotificationsListCtrl.js
@@ -0,0 +1,13 @@
+
+(function() {
+
+  "use strict";
+
+  var app = angular.module('cradair');
+
+  app.controller('NotificationsCtrl', function () {
+    console.log("Load NotificationsCtrl");
+  });
+
+
+})();
