d3422a83b3eae52d8150617bee2fba649b94fcc5
Jacques Belissent <jbelis@gmail.com>
2015-10-29 12:08:00 +0100

    added initial push code

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,27 @@
-// Ionic Starter App
+(function () {
+  "use strict";
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+  var app = angular.module('starter', ['ionic']);
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+  app.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+
+      var push = new Ionic.Push({
+        "debug": true
+      });
+
+      push.register(function (token) {
+        console.log("Device token:", token.token);
+      });
+      
+    });
   });
-})
+})();
